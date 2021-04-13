f0650ba9918ef320de524b91853c4f4bf6c4698d
sarayu96 <sarayu96@yahoo.com>
2021-04-13 17:09:53 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Detecting Button click
 let buttonsArray = document.querySelectorAll(".drum")
 
 for (let i = 0; i < buttonsArray.length; i++) {
@@ -6,8 +7,18 @@ for (let i = 0; i < buttonsArray.length; i++) {
 
 function handleClick() {
   let buttonInnerHtml = this.innerHTML;
+  makeSound(buttonInnerHtml);
+}
 
-  switch (buttonInnerHtml) {
+// Detecting keyboard press
+
+document.addEventListener("keydown", function(event){
+  let pressedKey = event.key;
+  makeSound(pressedKey);
+});
+
+function makeSound(key){
+  switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
@@ -40,40 +51,3 @@ function handleClick() {
       console.log(buttonInnerHtml);
   };
 }
-
-document.addEventListener("keydown",
-function(event){
-  let pressedKey = event.key;
-  switch (pressedKey) {
-    case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      var crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-    case "k":
-      var kick = new Audio("sounds/kick-bass.mp3");
-      kick.play();
-      break;
-    case "l":
-      var snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-    default:
-      console.log(buttonInnerHtml);
-  };
-});
